f3bdcbe3fb30e3790fc25ddafb5acc1ce0cad73f
MATTHEW BILL <bill.raharjo@gmail.com>
2023-04-23 22:56:42 +0700

    tugas minggu 8 validasi model api

--- Bookstoreapi/BookStoreApi/Models/Book.cs
+++ Bookstoreapi/BookStoreApi/Models/Book.cs
@@ -1,6 +1,7 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 namespace BookStoreApi.Models;
 
 public class Book
@@ -8,12 +9,11 @@ public class Book
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
-
     [BsonElement("Name")]
     [JsonPropertyName("Name")]
-
+    [Required]
     public string BookName { get; set; } = null!;
-
+    [Range(0, 999)]
     public decimal Price { get; set; }
 
     public string Category { get; set; } = null!;
